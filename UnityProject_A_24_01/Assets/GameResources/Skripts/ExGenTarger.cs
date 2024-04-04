9f4debcb18fec78114f59e309218392792b0962b
esuvean <esuvean@naver.com>
2024-04-04 15:58:03 +0900

    [Feat] Prefabs 생성 RayCast 까지 진행

---
+++ UnityProject_A_24_01/Assets/GameResources/Skripts/ExGenTarger.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExGenTarger : MonoBehaviour
+{
+    public GameObject Target;
+    public float checkTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkTime += Time.deltaTime;
+        if (checkTime >= 1.0f)
+        {
+            checkTime = 0.0f;
+            GameObject temp = Instantiate(Target);
+            temp.transform.position = new Vector3(-4.0f, -4.0f, 0.0f);
+            int RandomNumberX = Random.Range(0, 8);
+            int RandomNumberY = Random.Range(0, 8);
+            temp.transform.position += new Vector3(RandomNumberX, RandomNumberY, 0.0f);
+
+            Destroy(temp, 2.0f);
+        }
+    }
+}
