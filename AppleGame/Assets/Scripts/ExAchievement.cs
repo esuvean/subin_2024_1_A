70e7450a247e3b9b5451afb50ae4999ffa4ae4f7
esuvean <>
2024-06-13 14:32:25 +0900

    feat :  업적 달성 제작

---
+++ AppleGame/Assets/Scripts/ExAchievement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExAchievement : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            AchievementManager.instance.AddProgress("점프", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            AchievementManager.instance.AddProgress("하강", 1);
+        }
+    }
+}
